a590446230af5dc8c631e56c4aea0a4f81344d6c
Farzane2630 <farzanekazemi8517@gmail.com>
2025-04-21 11:37:28 +0330

    ref: lazy loading routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router";
-import Index from "./Pages/Index/Index";
-import Products from "./Pages/Products/Products";
-import About from "./Pages/About/About";
-import Blog from "./Pages/Blog/Blog";
-import Cart from "./Pages/Cart/Cart";
-import Checkout from "./Pages/Checkout/Checkout";
-import Contact from "./Pages/Contact/Contact";
-import ProductInfo from "./Pages/ProductInfo/ProductInfo";
-import WishList from "./Pages/WishList/WishList";
+
+
+const Index = lazy(() => import("./Pages/Index/Index"))
+const Products = lazy(() => import("./Pages/Products/Products"))
+const About = lazy(() => import("./Pages/About/About"))
+const Blog = lazy(() => import("./Pages/Blog/Blog"))
+const Cart = lazy(() => import("./Pages/Cart/Cart"))
+const Checkout = lazy(() => import("./Pages/Checkout/Checkout"))
+const Contact = lazy(() => import("./Pages/Contact/Contact"))
+const ProductInfo = lazy(() => import("./Pages/ProductInfo/ProductInfo"))
+const WishList = lazy(() => import("./Pages/WishList/WishList"))
 
 export default function AllRoutes() {
   return (
-    <Routes>
-      <Route path="/" Component={Index} />
-      <Route path="products/:currentPageCount" Component={Products} />
-      <Route path="about" Component={About} />
-      <Route path="blog" Component={Blog} />
-      <Route path="cart" Component={Cart} />
-      <Route path="checkout" Component={Checkout} />
-      <Route path="contact" Component={Contact} />
-      <Route path="product-info/:productID" Component={ProductInfo} />
-      <Route path="wishlist" Component={WishList} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" Component={Index} />
+        <Route path="products/:currentPageCount" Component={Products} />
+        <Route path="about" Component={About} />
+        <Route path="blog" Component={Blog} />
+        <Route path="cart" Component={Cart} />
+        <Route path="checkout" Component={Checkout} />
+        <Route path="contact" Component={Contact} />
+        <Route path="product-info/:productID" Component={ProductInfo} />
+        <Route path="wishlist" Component={WishList} />
+      </Routes>
+    </Suspense>
   );
 }
