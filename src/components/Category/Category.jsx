13be294b7d0f8a39ff2a8e240b4aab8169f80f2e
Farzane2630 <farzanekazemi8517@gmail.com>
2025-04-21 13:20:26 +0330

    ref: backgroundImage to image element

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,12 +1,15 @@
-import React from "react";
 import { Box, Button, Grid } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import "./_Category.scss";
-import { useSelector } from "react-redux";
 
 export default function Category() {
   const categories = useSelector((state) => state.categories);
+
+  const featuredCategoryOne = categories.slice(0, 2);
+  const featuredCategoryTwo = categories.slice(2, 4);
+
   return (
     <Box>
       <Grid
@@ -15,46 +18,55 @@ export default function Category() {
         direction="row"
         justifyContent="center"
       >
-        <Grid xs={12} md={3} className="grid-item">
-          {categories.slice(0, 2).map((category) => (
-            <div key={category.id} className="image-container">
-              <div
+        <Grid item xs={12} md={3} className="grid-item">
+          {featuredCategoryOne.map((category) => (
+            <div key={category.id} className="image-wrapper">
+              <img
+                src={category.cover}
+                alt={category.title}
+                loading="lazy"
                 className="image-container"
-                style={{ backgroundImage: `url(${category.cover})` }}
-              >
-                <div className="category-title">{category.title}</div>
-              </div>
+              />
+              <div className="category-title">{category.title}</div>
             </div>
           ))}
         </Grid>
-        <Grid xs={12} md={3} className="grid-item">
-          <div className="shop-part image-container">
-            <p className="landing-section-title">Vegetables</p>
-            <p className="landing-section-details">
-              protect the health of every home
-            </p>
-            <Button variant="contained" color="success">
-              <Link to="/products/1" className="link">
+        <Grid item xs={12} md={3} className="grid-item">
+          <div className="shop-image-wrapper">
+            <div className="shop-part">
+              <p className="landing-section-title">Vegetables</p>
+              <p className="landing-section-details">
+                protect the health of every home
+              </p>
+              <Button
+                component={Link}
+                to="/products/1"
+                variant="contained"
+                color="success"
+                className="link shop-now-btn"
+              >
                 Shop Now
-              </Link>
-            </Button>
-          </div>
-          <div className="image-container">
-            <div
+              </Button>
+            </div>
+            <img
+              src="/images/category.jpg"
+              alt="products-images"
+              loading="lazy"
               className="image-container"
-              style={{ backgroundImage: `url("/images/category.jpg")` }}
-            ></div>
+            />
           </div>
         </Grid>
-        <Grid xs={12} md={3} className="grid-item">
-          {categories.slice(2, 4).map((category) => (
-            <div className="image-container">
-              <div
+
+        <Grid item xs={12} md={3} className="grid-item">
+          {featuredCategoryTwo.map((category) => (
+            <div key={category.id} className="image-wrapper">
+              <img
+                src={category.cover}
+                alt={category.title}
+                loading="lazy"
                 className="image-container"
-                style={{ backgroundImage: `url(${category.cover})` }}
-              >
-                <div className="category-title">{category.title}</div>
-              </div>
+              />
+              <div className="category-title">{category.title}</div>
             </div>
           ))}
         </Grid>
